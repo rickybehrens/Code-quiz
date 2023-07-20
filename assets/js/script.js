dfd197f94091db058253acfc6854675ed0ce0ffd
Borito <behrensricardo@gmail.com>
2023-07-19 18:16:48 -0600

    After the pseudo code, I started with my css file and styling as well as some basic html lines for the opening style of the site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,43 @@
-// Pseudo Code
+// ## Pseudo Code
+//  * Home page has the following:
+//      ** "View Highscores" on the top left corner
+//      ** "Time: 0" on the top right corner
+//      ** Coding Quiz Challenge
+//      ** Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your scoretime by ten seconds!
+//      ** "Start Quiz" button
+
+//  * After the "Start Quiz" button is pressed, the following occurs:
+//      ** Timer changes from "Time: 0" to "Time: 75" and it starts counting down
+//      ** The first question appears with 4 multiple choice answers in button shape
+//      ** The moment the mouse hovers ove the image, it changes color to a little bit lighter
+//      ** When a "click" is made on the selected answer, the button gets a blue shade around it for 0.5 seconds
+//          *** Timer should stop
+//          *** If answer is correct, blue shade should turn green for 3 seconds
+//          *** "Correct!" should apprear below all the multiple choice answers for 3 seconds
+//          *** If asnwer is incorrect, blue shade should turn red and the correct answer's border should turn green for 3 seconds
+//          *** "Wrong!" should appear below all the multiple choice answers for 3 seconds
+//          *** Time in the timer should decrease by 10 seconds and it should turn red for 3 seconds
+//      ** After the 3 seconds past, the new question should appear automatically
+//      ** Timer should turn black again and continue with the count down
+//  * After 5 questions, the following message appears:
+//      ** All done!
+//      ** Your final score is 22 (those are the seconds remaining)
+//      ** Enter Initials: (foloowed by a field and a "Submit" button next to it)
+//      ** When box is selected, the borders change to blue
+//      ** When the mouse hovers on the button, it changes color to slightly lighter
+//      ** When the button is clicked, the shaded borders on the field box turn back to normal color but the border on the button change to the same blue color
+//      ** After the "click", the following message appears:
+//          *** Highscores
+//          *** Position, followed by initials, followed by score
+//          *** Two buttons below everything, "Go Back" and "Clear Highscores"
 
 // Variable
+
 
 // Function Definitions
 
 // Special Functions (like eventlisteners)
 
 // Business Logic (anything that can start the applications)
+
+startQuiz.addEventListener("click",)
